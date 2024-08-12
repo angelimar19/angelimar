fab9200c19814efd51279921e2a004e5cd7e236e
angelimar19 <morilloangelimar1@gmail.com>
2024-08-12 16:17:11 -0400

    provando tipos de datos y varibles

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-let variable = 1;
-let nombre = "angelimar"; 
+let suma = 2+3
+let resta = 5-6
+let mul = 2*63
+let division = 8/2
+
+let numE= 4+6
+let numD = 3*3.5
+let negativo = 2-3
+
+let nombre = " angelimar "
+let nulo = null
+let op
+
+let verdadero = 2<3
+let falso = 2>4
+let logicos = 26<30 && 15>=15
+let operador = 40 <= 6 || 20>10
+let negacion = !4 > 2
