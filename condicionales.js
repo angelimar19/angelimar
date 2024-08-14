f93b03160cb0d7cc163129e8f0b8996598332576
angelimar19 <morilloangelimar1@gmail.com>
2024-08-14 16:07:59 -0400

    clase de condiciones logicas

---
+++ condicionales.js
@@ -0,0 +1,41 @@
+// let puntuacion = 4
+// if (puntuacion > 7) {
+//     console.log('excelente')
+// }
+// else if (puntuacion >= 5 && puntuacion <= 7) {
+//     console.log('es media')
+// }else if(puntuacion>=3 && puntuacion<=4){
+//     console.log('mala')
+// }
+// else {
+//     console.log('pesima')
+// }
+
+let dia = 3
+switch (dia) {
+    case 1: console.log('lunes')
+
+        break;
+    case 2: console.log('martes')
+        break;
+    case 3: console.log('miercoles')
+        break;
+    case 4: console.log('jueves')
+        break;
+    case 5: console.log('viernes')
+        break;
+    case 6: console.log('sabado')
+        break;
+    case 7: console.log('domingo')
+        break
+
+    default:
+        console.log('esto no es un dia de la semana')
+        break;
+}
+ let edad = 21
+ let puedePasar
+
+ edad>18? puedePasar = true : puedePasar = false
+ console.log(puedePasar)
+
