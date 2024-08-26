8c34611e7be34e61bbcbb20ef2124f4469876698
angelimar19 <morilloangelimar1@gmail.com>
2024-08-26 16:09:45 -0400

    arreglos

---
+++ arrays.js
@@ -0,0 +1,21 @@
+/*let array=['angelimar','eleida','morillo','centeno']
+console.log(array[0])*/
+
+/*let datos=['angelimar', 17, 162, pizza]
+console.log(datos)*/
+
+let array=[true, 6, 'hola']
+array.push(24)          // el push agrega el 24 como el ultimo//
+console.log(array)
+
+let array=[true, 6, 'hola']
+array.pop()  //el pop elimina el ultimo valor//
+console.log(array)
+
+let array=[true, 6, 'hola']
+array.unshift(24) //igual que el push solo que lo agrega al principio//
+console.log(array)
+
+let array=[true, 6, 'hola']
+array.shift() // igual que el pop pero borra el primer elemento o valor//
+console.log(array)
---
+++ ciclos.js
@@ -0,0 +1,12 @@
+for (i = 1; i < 6; i++) {
+    console.log(i)
+}
+
+for (i = 1; i <= 10; i++) {
+    console.log('6x' + i + '=' + i *)
+}
+let acum = 0
+for (i = 1; i <= 10; i++) {
+    acum = acum + 1
+    console.log(acum)
+}
